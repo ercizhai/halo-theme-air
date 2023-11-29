9bd11aa31b74610f94bde9e3a0054942597a0009
ERCZ <ercizhai@gmail.com>
2023-11-30 00:31:44 +0800

    update layout.html and post.html

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
 import "./styles/main.css";
 import "./styles/tailwind.css";
 import "./styles/github-markdown.css";
+
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+type ColorSchemeType = "dark" | "light";
+export let currentColorScheme: ColorSchemeType = "light";
+if (isDarkModeEnabled()) {
+  currentColorScheme = "dark";
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
+  currentColorScheme = isDarkModeEnabled() ? "dark" : "light";
+});
+
+function isDarkModeEnabled() {
+  return window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+}
