eb1f1766810948da4eb66457e7ec1b3f6905e622
ERCZ <ercizhai@gmail.com>
2023-12-04 22:24:19 +0800

    update layout.html,index.html and post.html

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./styles/tailwind.css";
 import "./styles/github-markdown.css";
 
 import Alpine from "alpinejs";
+import * as tocbot from "tocbot";
 
 window.Alpine = Alpine;
 
@@ -20,4 +21,25 @@ window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () 
 
 function isDarkModeEnabled() {
   return window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+}
+
+export function generateToc() {
+  const content = document.querySelector(".markdown-body");
+  const titles = content?.querySelectorAll("h1, h2, h3, h4");
+  if (!titles || titles.length === 0) {
+    const tocContainer = document.querySelector(".toc-container");
+    tocContainer?.remove();
+    return;
+  }
+  tocbot.init({
+    tocSelector: ".toc",
+    contentSelector: ".markdown-body",
+    headingSelector: "h1, h2, h3, h4",
+    activeLinkClass: "toc-active-link",
+    extraLinkClasses: "text-base text-slate-600 inline-block my-1 hover:text-sky-400",
+    extraListClasses: "ml-[1em]",
+    collapseDepth: 6,
+    headingsOffset: 100,
+    scrollSmooth: true,
+  });
 }
